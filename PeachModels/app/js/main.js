d90b5909e21fdfe7c4a2eeb4ac9753bd1bb3dce5
Den <shpirniy008@gmail.com>
2020-04-20 02:52:24 +0300

    update

--- PeachModels/app/js/main.js
+++ PeachModels/app/js/main.js
@@ -142,6 +142,32 @@ window.onload = function() {
 };
 
 
+(function() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.animation-container');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop+200 - windowHeight <= 0) {
+        element.classList.add('active');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+})();
 
 
 
