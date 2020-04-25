5eba83c191608dbadf3fba082ffac233556582a6
Den <shpirniy008@gmail.com>
2020-04-25 22:28:33 +0300

    update

---
+++ AntiOne/app/js/main.js
@@ -0,0 +1,42 @@
+
+// For popup
+function clickItemHandler(event) {
+  if (!event.target.closest('.click-item')) return;
+  let item = event.target.closest('.click-item');
+
+  let obj = {
+    'toggle': function (target) {
+      target.closest('.click-obj').classList.toggle('active');
+    },
+
+    'remove': function (target) {
+      target.closest('.click-obj').remove();
+    },
+
+    'popup-open': function (target) {
+      if (!document.querySelector(target.dataset.label)) return;
+      document.querySelector(target.dataset.label).classList.add('active');
+    },
+
+    'popup-close': function (target) {
+      if (target.dataset.label) {
+        document.querySelector(target.dataset.label).classList.remove('active')
+      } else {
+        target.closest('.popup_container').classList.remove('active');
+      }
+    },
+  }
+
+  if (item.dataset.action) {
+    let actions = item.dataset.action.split(' ');
+    actions.forEach(action => obj[action](item))
+  } else {
+    obj['toggle'](item);
+  }
+};
+document.addEventListener('click', clickItemHandler);
+
+
+
+
+
