3eee95146bd98b3af68e2e93f31426e29f205fb9
Den <shpirniy008@gmail.com>
2020-10-13 18:04:35 +0300

    update

--- FoodFamily/app/js/main.js
+++ FoodFamily/app/js/main.js
@@ -1,4 +1,46 @@
-  $('.in-link').click(function(event){
+function clickItemHandler(event) {
+  if (!event.target.closest('.click-item')) return;
+  let item = event.target.closest('.click-item');
+
+  let obj = {
+    'toggle': function (target) {
+      target.closest('.click-obj').classList.toggle('active');
+    },
+
+    'remove': function (target) {
+      target.closest('.click-obj').remove();
+    },
+
+    'popup-open': function (target) {
+      if (!document.querySelector(target.dataset.label)) return;
+      document.querySelector(target.dataset.label).classList.add('active');
+    },
+
+    'menu-open': function (target) {
+      target.closest('.header__cabinet__menu').classList.toggle('active');
+    },
+
+    'popup-close': function (target) {
+      if (target.dataset.label) {
+        document.querySelector(target.dataset.label).classList.remove('active')
+      } else {
+        target.closest('.popup_container').classList.remove('active');
+      }
+    },
+  }
+
+  if (item.dataset.action) {
+    let actions = item.dataset.action.split(' ');
+    actions.forEach(action => obj[action](item))
+  } else {
+    obj['toggle'](item);
+  }
+};
+document.addEventListener('click', clickItemHandler);
+
+
+
+  $('.in-link').click(function (event) {
     event.preventDefault();
     if ($(this).hasClass('registration')) {
       $('.cabinet-section__container.registration').show();
