a8f082765a97202338b376f5ee74b4f7c2ca5d44
Den <shpirniy008@gmail.com>
2020-10-13 22:55:43 +0300

    update

--- GoMarketing/app/js/main.js
+++ GoMarketing/app/js/main.js
@@ -1,60 +1,32 @@
+$(function () {
+  // For smooth scrolling to anchors 
 
-function clickItemHandler(event) {
-  if (!event.target.closest('.click-item')) return;
-  let item = event.target.closest('.click-item');
+  $(document).ready(function () {
+    $("#request-form").submit(function () { //Change
+      var th = $(this);
+      $.ajax({
+        type: "POST",
+        url: "mail.php", //Change
+        data: th.serialize()
+      }).done(function () {
+        window.location.href = "ty.html";
+      });
+      return false;
+    });
+  });
 
-  let obj = {
-    'toggle': function (target) {
-      target.closest('.click-obj').classList.toggle('active');
-    },
+  $(".banner__link").click(function (event) {
+    event.preventDefault();
+    var destination = $('.request__wrapp').offset().top + 50;
+    $('html').animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
 
-    'remove': function (target) {
-      target.closest('.click-obj').remove();
-    },
+  $('input.phone').inputmask('+389999999999', {
+    "clearIncomplete": true
+  });
 
-    'popup-open': function (target) {
-      if (!document.querySelector(target.dataset.label)) return;
-      document.querySelector(target.dataset.label).classList.add('active');
-    },
 
-    'menu-open': function (target) {
-      target.closest('.header__cabinet__menu').classList.toggle('active');
-    },
-
-    'popup-close': function (target) {
-      if (target.dataset.label) {
-        document.querySelector(target.dataset.label).classList.remove('active')
-      } else {
-        target.closest('.popup_container').classList.remove('active');
-      }
-    },
-  }
-
-  if (item.dataset.action) {
-    let actions = item.dataset.action.split(' ');
-    actions.forEach(action => obj[action](item))
-  } else {
-    obj['toggle'](item);
-  }
-};
-document.addEventListener('click', clickItemHandler);
-
-// For header-language
-let languageWrapp = document.querySelector('.header__language');
-let languageToggle = document.querySelector('.header__language__dropdown__toggle');
-let languageDropdown = document.querySelector('.header__language__dropdown__container')
-
-let toggleDropdownLanguage = function () {
-  languageDropdown.classList.toggle('show');
-  languageWrapp.classList.toggle('active');
-}
-
-languageToggle.addEventListener('click', toggleDropdownLanguage);
-
-$(document).mouseup(function (e) {
-  let container = $(".header__language");
-  if (container.has(e.target).length === 0) {
-    languageDropdown.classList.remove('show');
-    languageWrapp.classList.remove('active');
-  }
 });
